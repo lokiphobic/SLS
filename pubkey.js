4fa47bcd5c4db42f129c652487aecc7e00093dcb
hacky <58670085+lokiphobic@users.noreply.github.com>
2022-07-30 06:07:42 -0400

    Delete pubkey.js

--- pubkey.js
+++
@@ -1,7 +0,0 @@
-//024b1f5fdd155fa387cf5438d245677c978709c83fceddc2a5ea0e69cff326a993
-
-const bitcoinjs = require( 'bitcoinjs-lib' );
-
-const pubkey = Buffer.from( '024b1f5fdd155fa387cf5438d245677c978709c83fceddc2a5ea0e69cff326a993', 'hex' );
-const { address } = bitcoinjs.payments.p2pkh({ pubkey });
-console.log( address ); // 1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs
